2dd09ebcfb57933867cae38017141c5c79895278
Fabian Ghena <fabian_costin@yahoo.com>
2022-11-30 15:51:26 +0200

    implemented the password length functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const minusBtnElement = document.getElementById('minus');
+const plusBtnElement = document.getElementById('plus');
+const lengthOutputElement = document.getElementById('length-output');
+
+let passwordLength = Number(lengthOutputElement.textContent);
+
+// Password Length
+minusBtnElement.addEventListener('click', () => {
+  if (passwordLength > 1) {
+    passwordLength--;
+    lengthOutputElement.textContent = passwordLength;
+  }
+});
+
+plusBtnElement.addEventListener('click', () => {
+  if (passwordLength < 10) {
+    passwordLength++;
+    lengthOutputElement.textContent = passwordLength;
+  }
+});
